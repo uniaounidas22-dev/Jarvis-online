6b2ace686096d32ed899c8ab7906ab513b6c3bc7
uniaounidas22-dev <uniaounidas22@gmail.com>
2026-09-05 12:14:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 from kivy.core.window import Window
+import requests
 
 Window.clearcolor = (0.05, 0.05, 0.1, 1)
 
-class LoginScreen(BoxLayout):
-    def __init__(self, app, **kwargs):
+class LoginScreen(Screen):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.orientation = 'vertical'
-        self.padding = 30
-        self.spacing = 20
-        self.app = app
-
-        self.add_widget(Label(text='[b]JARVIS V6 ONLINE[/b]', markup=True, font_size='28sp', color=(0, 1, 1, 1)))
-        self.add_widget(Label(text='Sistema 100% Online', font_size='16sp', color=(0.7, 0.7, 0.7, 1)))
-
-        self.user = TextInput(hint_text='Usuario: jarvis', multiline=False, size_hint_y=None, height=50)
-        self.passw = TextInput(hint_text='Senha: JARVIS-2026', password=True, multiline=False, size_hint_y=None, height=50)
-        self.add_widget(self.user)
-        self.add_widget(self.passw)
-
-        self.msg = Label(text='', color=(1, 0.3, 0.3, 1
+        layout = BoxLayout(orientation='vertical', padding=30, spacing=15)
+        layout.add_widget(Label(text='J.A.R.V.I.S V6 ONLINE', font_size=28, bold=True, color=(0,0.8,1,1)))
+        layout.add_widget(Label(text='Acesso Restrito - Tony Stark', font_size=14, color=(0.7,0.7,0.7,1)))
+        self.user = TextInput(hint_text='Usuario', multiline=False, size_hint_y=None, height=50)
+        self.pwd = TextInput(hint_text='Senha', password=True, multiline=False, size_hint_y=None, height=50)
+        self.msg = Label(text='Usuario: jarvis | Sen
